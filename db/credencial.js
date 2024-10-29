c925aea4a5a62f5c634ddea95f53b8840754564d
unknown <sofiaavelz004@gmail.com>
2024-10-28 22:02:08 -0500

    uwu

---
+++ db/credencial.js
@@ -0,0 +1,7 @@
+// db/credenciales.js
+const usuarios = [
+    { username: 'sofia', password: 'sofiauwu', role: 'user' },
+    { username: 'admin', password: 'adminuwu', role: 'admin' }
+];
+
+module.exports = usuarios;
